c395be11bd11516f9a7c2fecf215d082837b3cb0
technophilist <54663474+technophilist@users.noreply.github.com>
2024-12-02 13:24:11 -0500

    add #setPublishStatusForQuiz & #deleteQuiz to quizzes/dao.js

--- kanbas/quizzes/dao.js
+++ kanbas/quizzes/dao.js
@@ -4,4 +4,14 @@ const getQuizSummariesForCourse = (courseId) => {
     return database.quizzes.filter(quiz => quiz.courseId === courseId)
 }
 
-export default {getQuizSummariesForCourse}
+const setPublishStatusForQuiz = (quizId, isPublished) => {
+    console.log("Setting publish status for quiz", quizId, isPublished)
+    const quiz = database.quizzes.find(q => q.id === quizId)
+    quiz.isPublished = isPublished
+}
+
+const deleteQuiz = (quizId) => {
+    database.quizzes = database.quizzes.filter(q => q.id !== quizId)
+}
+
+export default { getQuizSummariesForCourse, setPublishStatusForQuiz, deleteQuiz }
