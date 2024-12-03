a6b91f93e246fcc844866db62c0670e8ab7a37c7
technophilist <54663474+technophilist@users.noreply.github.com>
2024-12-03 17:28:21 -0500

    add route to update all questions of a quiz

--- kanbas/quizzes/routes.js
+++ kanbas/quizzes/routes.js
@@ -1,6 +1,6 @@
 import quizzesDao from "./dao.js"
 function QuizzesRoutes(app) {
-
+    // get all quiz summaries for a course
     app.get("/api/quizzes/summary/:courseId", async (req, res) => {
         const { courseId } = req.params
         const quizzes = quizzesDao
@@ -17,24 +17,28 @@ function QuizzesRoutes(app) {
         res.json(quizzes)
     })
 
+    // get a quiz by id
     app.get("/api/quizzes/:quizId", (req, res) => {
         const { quizId } = req.params
         const quiz = quizzesDao.getQuizById(quizId)
         res.json(quiz)
     })
 
+    // get all questions for a quiz
     app.get("/api/quizzes/:quizId/questions", (req, res) => {
         const { quizId } = req.params
         const questions = quizzesDao.getQuestionsForQuiz(quizId)
         res.json(questions)
     })
 
+    // update a quiz
     app.put("/api/quizzes/:quizId", (req, res) => {
         const updatedQuiz = req.body
         quizzesDao.updateQuiz(updatedQuiz)
         res.json(200)
     })
 
+    // update a question
     app.put("/api/quizzes/:quizId/questions/:questionId", (req, res) => {
         const { quizId, questionId } = req.params
         const updatedQuestion = req.body
@@ -42,6 +46,15 @@ function QuizzesRoutes(app) {
         res.json(200)
     })
 
+    // update questions of a quiz
+    app.put("/api/quizzes/:quizId/questions", (req, res) => {
+        const { quizId } = req.params
+        const updatedQuestionList = req.body
+        quizzesDao.updateQuestionsOfQuiz(quizId, updatedQuestionList)
+        res.json(200)
+    })
+
+    // set publish status for a quiz
     app.patch("/api/quizzes/:quizId", (req, res) => {
         const { quizId } = req.params
         const { isPublished } = req.body
@@ -49,6 +62,7 @@ function QuizzesRoutes(app) {
         res.json(200)
     })
 
+    // delete a quiz
     app.delete("/api/quizzes/:quizId", (req, res) => {
         const { quizId } = req.params
         quizzesDao.deleteQuiz(quizId)
